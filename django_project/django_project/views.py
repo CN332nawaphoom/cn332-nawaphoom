08299a0cd6c81b655813df1b0d1fdd87a47c9363
suttha jongjaroen <6410615121@cn330>
2024-02-12 21:13:20 +0700

    navbar

--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def dashboard(request):
+    context = {'test_word': "hello_world"}
+
+    return render(request, "dashboard.html", context)
