ee16526927958302efb86f8b913a55141f24747d
suttha jongjaroen <6410615121@cn330>
2024-03-08 10:24:33 +0700

    remove index view

--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -3,13 +3,6 @@ from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-
-
-@login_required(login_url='login')
-def index(request):
-    return render(request, "main/index.html")
-
-
 def login(request):
     return render(request, "account/login.html")
 
@@ -21,4 +14,8 @@ def logout_views(request):
 def dashboard(request):
     context = {'test_word': "hello_world"}
     return render(request, "main/dashboard.html",context)
+
+@login_required
+def profile(request):
+    return render(request, "main/profile.html")
 
